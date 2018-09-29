853c0e45dbd9ee48730f55eda5d2c556f95b4011
eylonronen <eylon.ronen@gmail.com>
2018-09-29 21:58:46 +0300

    Create module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import VirtualList from "react-tiny-virtual-list";
 
-import styles from './styles.css'
+const escapeRegex = st => st.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+export default ({ text, rowSeperator, rowLength, rowRender, ...listProps }) => {
+  rowLength = rowLength || 120;
+  rowSeperator = escapeRegex(rowSeperator) || "\n";
+  let reg = `([^${rowSeperator}]{1,${rowLength}}|${rowSeperator}(?=${rowSeperator}))`;
+  let rows = text.match(new RegExp(reg, "g"));
+  return (
+    <VirtualList
+      itemCount={rows.length}
+      renderItem={({ style, index }) => (
+        <div style={{ ...style, whiteSpace: "nowrap" }} key={index}>
+          {rowRender ? rowRender(rows[index]) : rows[index]}
+        </div>
+      )}
+      {...listProps}
+    />
+  );
+};
